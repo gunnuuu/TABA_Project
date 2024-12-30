8779e30d987f96a7a720c1bd23cf2f8403c62afe
Dev.gunnuuu <105582355+gunnuuu@users.noreply.github.com>
2024-12-30 15:23:50 +0900

    Update InfoRepository.java

--- src/main/java/com/example/taba_project/repository/InfoRepository.java
+++ src/main/java/com/example/taba_project/repository/InfoRepository.java
@@ -1,8 +1,8 @@
 package com.example.taba_project.repository;
 
-import com.example.taba_project.model.Info2;
+import com.example.taba_project.model.Info;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface Info2Repository extends JpaRepository<Info2, Long> {
-    Info2 findFirstByOrderByCreatedAtDesc(); // createdAt 기준 내림차순 정렬 후 첫 번째 데이터 반환
+public interface InfoRepository extends JpaRepository<Info, Long> {
+    Info findFirstByOrderByCreatedAtDesc(); // createdAt 기준 내림차순 정렬 후 첫 번째 데이터 반환
 }
