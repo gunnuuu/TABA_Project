9cb42a10c58ca765b67df35d21c9f5724491e5aa
Dev.gunnuuu <105582355+gunnuuu@users.noreply.github.com>
2024-12-30 15:23:27 +0900

    Update ImageRepository.java

--- src/main/java/com/example/taba_project/repository/ImageRepository.java
+++ src/main/java/com/example/taba_project/repository/ImageRepository.java
@@ -2,6 +2,10 @@ package com.example.taba_project.repository;
 
 import com.example.taba_project.model.Image;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ImageRepository extends JpaRepository<Image, Long> {
+    // 최신 이미지를 가져오는 쿼리 메서드
+    @Query(value = "SELECT * FROM image i ORDER BY i.id_images DESC LIMIT 1", nativeQuery = true)
+    Image findLatestImage();
 }
